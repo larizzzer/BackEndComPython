a410a45d6715938436400d5ea16adf2dd461a570
Larissa Gomes <larissagomesgaspar@gmail.com>
2025-10-29 23:10:26 -0300

    Variaveis e Constantes
    Define-se o que são variáveis e constantes

---
+++ ArquivosPython/VariaveiseConstantes.py
@@ -0,0 +1,12 @@
+nome = 'Larissa'
+idade = 23
+
+nome, idade = 'Iolanda', 32
+nome, idade = ('Sally', 21) #uma das formas de se declarar váriavel, funciona também sem as (), e sempre mostra o último a ser declarado
+print(nome, idade)
+
+limite_saque_diario = 1000
+
+BRAZILLIAN_STATES = ['SP', 'RJ', 'RS', 'SC'] #Convenção, não altera seu valor até o final do programa
+
+print(BRAZILLIAN_STATES)
