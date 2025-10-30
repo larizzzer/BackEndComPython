19504d8a9d262d82fc802607829ccc95c5a7c8aa
Larissa Gomes <larissagomesgaspar@gmail.com>
2025-10-30 17:58:00 -0300

    Operadores Aritméticos
    Mostra os operadores aritiméticos que são usados em python para fazer contas e mostra a procedencia deles (ordem que são realizados)

---
+++ ArquivosPython/OperadoresAritmeticos.py
@@ -0,0 +1,15 @@
+produto1 = 20
+produto2 = 10
+
+print(produto1 + produto2) # soma
+print(produto1 - produto2) # subtração
+print(produto1 / produto2) # divisão
+print(produto1 // produto2) # divisão inteira
+print(produto1 * produto2) # multiplicação
+print(produto1 % produto2) # módulo (resto da divisão)
+print(produto1 ** produto2) # exponenciação
+
+x = 10 + 5 * 4
+y = (10 / 2) + 25 * ((2 - 2) ** 2)
+print(x)
+print(y)
