20977b4de8b5cb26ae7342395be2d50d7b8874d5
Larissa Gomes <larissagomesgaspar@gmail.com>
2025-11-03 15:24:10 -0300

    Operadores de Atribuição
    Explicação dos operadores de atribuição, sendo os simples e os de atribuição combinada.

---
+++ ArquivosPython/OperadoresAtribuicao.py
@@ -0,0 +1,26 @@
+saldo = 500 #atribuição simples
+print(saldo)
+
+saldo = 200
+print(saldo)
+
+saldo += 10  #soma
+print(saldo)
+
+saldo -= 5   #subtrai
+print(saldo)
+
+saldo //= 2  #divisão inteira --> mais usado para quando tiver só números inteiros
+print(saldo)
+
+saldo /= 2   #divide (vira float)
+print(saldo)
+
+saldo *= 10  #multiplica
+print(saldo)
+
+saldo %= 4   #resto da divisão
+print(saldo)
+
+saldo **= 2  #exponeciação
+print(saldo)
