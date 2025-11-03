1719782bfcd32457a0e4d34f4df93b135250f81a
Larissa Gomes <larissagomesgaspar@gmail.com>
2025-11-03 17:10:04 -0300

    Operadores de Associação
    Explicação rápida sobre os operadores de associação e de como usá-los.

---
+++ ArquivosPython/OperadoresAssociacao.py
@@ -0,0 +1,7 @@
+frutas = ['limão', 'uva', 'tomate', 'maçã']
+curso = 'Curso de python'
+
+print('laranja' not in frutas) #retorna TRUE pois laranja não pertence a frutas
+print('limão' in frutas) #retorna TRUE pois limão pertence a frutas
+print('Python' in curso) #retorna FALSE pois Python não está de letra maiúscula
+print('uva' not in frutas) #retorna FALSE pois uva pertence a frutas
