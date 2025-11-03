9f844fbb5866bd1bd523b5d3fa83014ab344d057
Larissa Gomes <larissagomesgaspar@gmail.com>
2025-11-03 16:10:45 -0300

    Operadores de Identidade
    Explicação de como funciona os operadores de identidade.

--- ArquivosPython/OperadoresIdentidade.py
+++ ArquivosPython/OperadoresIdentidade.py
@@ -1,4 +1,5 @@
 saldo = 1000
-limite = 500
+limite = 1000
 
-print(saldo is limite)
+print(saldo is limite) #retorna TRUE se os dois operandos se referem ao mesmo objeto
+print(saldo is not limite) #retorna TRUE se os dois operandos se não referem ao mesmo objeto
