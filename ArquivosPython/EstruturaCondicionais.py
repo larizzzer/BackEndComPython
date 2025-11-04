33213ad0354964f1eef5adfa2df3984fc1905a23
Larissa Gomes <larissagomesgaspar@gmail.com>
2025-11-04 16:13:36 -0300

    Estrutura condicionais
    Explicação de como funcionaria um IF aninhado, com vários IFs, ELIFs e ELSEs.

--- ArquivosPython/EstruturaCondicionais.py
+++ ArquivosPython/EstruturaCondicionais.py
@@ -21,3 +21,33 @@ elif idade == IDADE_ESPECIAL: #há um teste e se caso retorna TRUE, o bloco dent
     print('Pode fazer as aulas teóricas, mas não pode fazer as aulas práticas!')
 else:
     print('Ainda não pode tirar a CNH.')
+
+#IF aninhado
+conta_normal = False
+conta_universitaria = False
+conta_especial = True
+saldo = 2000
+saque = 1600
+cheque_especial = 450
+
+if conta_normal:
+
+    if saldo >= saque:
+        print('Saque realizado com sucesso!')
+    elif saque <= (saldo + cheque_especial):
+        print('Saque realizado com o cheque especial!')
+    else:
+        print('Não é possível realizar o saque, saldo insuficiente!')
+
+elif conta_universitaria:
+
+    if saldo >= saque:
+        print('Saque realizado com sucesso!')
+    else:
+        print('Saldo insuficiente!')
+
+elif conta_especial:
+    print('Conta especial selecionada!')
+
+else:
+    print('Sistema não reconheceu seu tipo de conta, entre em contato com o seu gerente!')
