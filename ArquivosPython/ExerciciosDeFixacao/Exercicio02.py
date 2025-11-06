20853ba754da09adc98946b6b32accbb62e1769f
Larissa Gomes <larissagomesgaspar@gmail.com>
2025-11-06 17:24:20 -0300

    Exercício 02
    O Exercicio 02 verifica se um número é par ou ímpar.

---
+++ ArquivosPython/ExerciciosDeFixacao/Exercicio02.py
@@ -0,0 +1,8 @@
+# Verificação de números pares e números ímpares
+
+numero = int(input('Digite um número inteiro: '))
+
+if numero % 2 == 0:
+    print(f'O número {numero} é par!')
+else:
+    print(f'O número {numero} é ímpar!')
