c88b695213eec15499ec9d4631b5f81d82f935ef
Larissa Gomes <larissagomesgaspar@gmail.com>
2025-11-13 14:49:57 -0300

    Remoção de espaços
    Utilização de strip(), lstrip() e rstrip().

--- ArquivosPython/String_1.py
+++ ArquivosPython/String_1.py
@@ -3,3 +3,10 @@ nome = 'LaRIsSa'
 print(nome.upper()) # --> Tudo MAIÚSCULO
 print(nome.lower()) # --> Tudo minúsculo
 print(nome.title()) # --> Deixa a primeira letra Maiúscula
+
+texto = '   Hello World!     '
+
+print(texto)
+print(texto.strip())  # --> Remove todos os espaçoes
+print(texto.lstrip()) # --> Remove os espaços da esquerda
+print(texto.rstrip()) # --> Remove os espaços da direita
