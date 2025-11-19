37edc8b5f3599550156edd190a925a17a80d98b1
Larissa Gomes <larissagomesgaspar@gmail.com>
2025-11-19 12:23:32 -0300

    Múltiplas linhas
    Explicação de como funciona a string de múltiplas linhas

---
+++ ArquivosPython/String_4.py
@@ -0,0 +1,11 @@
+nome = 'Larissa'
+
+mensagem = f'''
+    Olá, meu nome é {nome},
+    e eu estou aprendendo python
+    para quando eu for aprender mais sobre análise de dados,
+    eu já saber bastante sobre a linguagem.
+    Essa mensagem tem diferentes recursos.
+''' # --> String multi linhas, onde usa 3 aspas simples ou duplas
+
+print(mensagem)
