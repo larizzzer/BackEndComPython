5ed8d3857209d3f4c468efbd42f32712490eaf6c
Larissa Gomes <larissagomesgaspar@gmail.com>
2025-11-19 11:51:41 -0300

    Fatiamento
    Explicação de como funciona o fatiamento de strings

---
+++ ArquivosPython/String_3.py
@@ -0,0 +1,10 @@
+nome = 'Larissa Gomes Gaspar'
+
+print(nome[0]) # --> retorna o primeiro caracter
+print(nome[-1]) # --> retorna o último caracter
+print(nome[:8]) # --> considera todo o início e para no caracter 7
+print(nome[8:]) # --> considera a partir do caracter 8 e vai até o final
+print(nome[8:20]) # --> faz uma substring pegando do caracter 8 até o 19
+print(nome[8:20:2]) # --> faz uma substring pegando do caracter 8 até o 19 e pula de 2 em 2
+print(nome[:]) # --> retorna a string inteira
+print(nome[::-1]) # --> retorna a string inteira de trás para frente, como um espelhamento
